2f139af397d5a70547fa13c20281040a401768a1
Glitch (fierce-beach) <none>
2017-11-19 09:08:55 +0000

    💘🐌 Updated with Glitch

--- server.js
+++ server.js
@@ -4,9 +4,13 @@
 // init project
 var express = require('express');
 var app = express();
-var result = {};
 app.use(express.static('public'));
 
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGOLAB_URI;
+
+var result = {};
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -14,26 +18,78 @@ app.get("/", function (req, res) {
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get(/^\/new\/((https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?(:\d+)*$)/, function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-<<<<<<< HEAD
-    result = {new_id: req.params[0]};
- // res.write(JSON.stringify(result));
-  res.end(JSON.stringify(result));
+  
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      result = {id: 'Unable to connect to the mongoDB server. Error:'};
+      res.end(JSON.stringify(result));
+    } else {
+      console.log('Connection established to', url);
+      var col = db.collection("urls");
+      col.count( (err, count) => {
+        if (err) {
+          result = {id: 'Unable to count documets'};
+          res.end(JSON.stringify(result));
+        } else {
+          console.log(count)
+          count++;
+          var shortUrl = {
+            url: req.params[0],
+            short: count
+          };
+          col.insert(shortUrl, {safe: true}, (err, doc) => {
+            if (err) {
+              result = {id: 'Unable to insert documet'};
+              res.end(JSON.stringify(err));           
+            } else  {
+              delete shortUrl._id;
+              shortUrl.short = req.headers.host + "/" + shortUrl.short
+              res.writeHead(200, { 'Content-Type': 'application/json' });            
+              res.end(JSON.stringify(shortUrl));
+              db.close();
+            }          
+          })         
+        }
+      });
+      
+    }
+  });
+
 });
 app.get(/new/, function (req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-    result = {error: "wrong url"};
-=======
-    result = {new_id: req.params.id};
->>>>>>> 11c7d6d907609aff47a1f1b088c2ac282d3474bc
- // res.write(JSON.stringify(result));
+  result = {error: "wrong url"};
   res.end(JSON.stringify(result));
 });
-app.get("/:id", function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  result = {id: req.params.id};
- // res.write(JSON.stringify(result));
-  res.end(JSON.stringify(result));
+app.get(/^\/(\d+$)/, function (req, res) {
+  var id = req.params[0]
+  console.log(id)
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      result = {id: 'Unable to connect to the mongoDB server. Error:'};
+      res.end(JSON.stringify(result));
+    } else {
+      console.log('Connection established to', url);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      result = {id: id, status: 'Connection established'};
+      res.end(JSON.stringify(result));
+      db.close();
+    }
+  });
+
+});
+
+app.get(/.*/, function (req, res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  result = "Page not found";
+  res.end(result);
 });
 
 // listen for requests :)
